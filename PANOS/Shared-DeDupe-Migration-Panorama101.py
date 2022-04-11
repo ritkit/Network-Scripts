b79d10c68441387f6bf3f6a0ff459de3e5a56a06
Christian RAhl <crahl@kalsec.com>
2022-04-11 13:56:05 -0400

    Continued updates

--- PANOS/Shared-DeDupe-Migration-Panorama101.py
+++ PANOS/Shared-DeDupe-Migration-Panorama101.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Shared-DeDupe-Migration-Panorama101.p
 #
 # Description
@@ -10,10 +12,18 @@
 # Writer - Christian Rahl
 
 import argparse
+from curses import can_change_color
 import requests
 import yaml
 
-parser = argparse.ArgumentParser(description="Run through Palo Alto Panorama 
-    to find duplicates across sites and merge them/punt them up to a shared policy")
+parser = argparse.ArgumentParser(description="Run through Palo Alto Panorama to find
+    duplicates across sites and merge them/punt them up to a shared policy")
 
-parser.add_argument('-f', '--configfile', )
+parser.add_argument('-u', '--user', help='Provide username for accessing palo altos,
+    without, user will be prompted, or yaml config checked')
+parser.add_argument('-p', '--password' help='Provide the password, recommended is to
+     exclude this flag as the user will be prompted in a more secure way. Yaml can
+     be used also but also not recommended.')
+parser.add_argument('-f', '--configfile', ,help='Yaml Config for modifying policies'
+     required=True)
+
