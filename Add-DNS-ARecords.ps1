28746300cc16a39734805b9c2691436bb8cf55e8
Kit <crahl@christianrahl.com>
2024-11-26 11:19:03 -0600

    Add DNS update PowerShell script

---
+++ Add-DNS-ARecords.ps1
@@ -0,0 +1,8 @@
+﻿#Import CSV
+$IPS = Import-CSV "C:\Users\ItIsMe\Downloads\IPList.csv"
+
+foreach($IP in $IPS){
+
+    Add-DnsServerResourceRecordA -ZoneName "test.local" -ComputerName "inf-pvw-addc01.grubhub.local" -Name $IP.name  -CreatePtr -IPv4Address $IP.ip
+
+}
